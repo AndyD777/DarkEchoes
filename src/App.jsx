73cc374b82c6f4fa72d752087584fb880232c323
Andrew Depradine <andy.depradine@yahoo.com>
2025-04-30 18:55:29 -0400

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,39 @@
+import { useState } from "react";
+import { episodeList } from "./data";
+import "./index.css"; 
+
 export default function App() {
-  // TODO
+  //TODO
+  
+  const [episodes] = useState(episodeList);
+  const [selectedId, setSelectedId] = useState(null);
+  const selectedEpisode = episodes.find((ep) => ep.id === selectedId);
+
+  return (
+    <div className="container">
+      <h1>Dark Echoes</h1>
+
+      <ul className="episode-list">
+        {episodes.map((episode) => (
+          <li key={episode.id} className="episode-item">
+            <button
+              className={`episode-button ${
+                selectedId === episode.id ? "active" : ""
+              }`}
+              onClick={() => setSelectedId(episode.id)}
+            >
+              {episode.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      {selectedEpisode && (
+        <div className="episode-details">
+          <h2>{selectedEpisode.title}</h2>
+          <p>{selectedEpisode.description}</p>
+        </div>
+      )}
+    </div>
+  );
 }
